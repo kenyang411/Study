554a9eeb73687b3036e37b7e270cfe70e9f2fd82
kenyang411 <57252183+kenyang411@users.noreply.github.com>
2020-01-08 19:11:01 +0800

    AOP tx

--- SpringStudy/Spring03/src/tx/annotation/test/TestTransaction.java
+++ SpringStudy/Spring03/src/tx/annotation/test/TestTransaction.java
@@ -1,10 +1,28 @@
 package tx.annotation.test;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import tx.annotation.dao.BookShopDao;
+import tx.annotation.service.BookShopService;
 
 public class TestTransaction {
-    @Test
-    public void test(){
 
+
+    private BookShopDao bookShopDao;
+
+    private BookShopService bookShopService;
+
+    @Before
+    public void init(){
+        ApplicationContext ctx= new ClassPathXmlApplicationContext("spring-tx.xml");
+        bookShopDao=ctx.getBean("bookShopDaoImpl",BookShopDao.class);
+        bookShopService=ctx.getBean("bookShopServiceImpl",BookShopService.class);
+    }
+
+    @Test
+    public void testTx(){
+    bookShopService.buyBook("Tom","ISBN-001");
     }
 }

ccf5bea459ee6338add4293181d22c51f161b4b5
kenyang411 <57252183+kenyang411@users.noreply.github.com>
2020-01-23 18:22:30 +0800

    interceptor

---
+++ SpringMVCStudy/springCURD/src/interceptor/MyFirstInterceptor.java
@@ -0,0 +1,29 @@
+package interceptor;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class MyFirstInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println("MyFirstInterceptor preHandle");
+        return true;
+        //return false;   //拦截到 return false。Http请求中断。不显示结果。
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("MyFirstInterceptor postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.println("MyFirstInterceptor afterCompletion");
+    }
+}
